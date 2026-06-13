333a052c641bc9c1fc63b63d656300c943e9001a
boss-vinci <vinciisaboss2008@gmail.com>
2026-06-13 16:09:03 +0100

    Initial commit

---
+++ DataType.java
@@ -0,0 +1,27 @@
+public class DataType{
+	public static void main(String[] args){
+		byte age = 25;
+		System.out.printf("You are %d years old%n",age);
+		
+		short quantity = 30000;
+		system.out.printf("I bought %,d quantity of phones%n",qantity);
+		
+		int population = 2025278268;
+		System.out.printf("The population in Nigeria is %,d%n",population);
+		
+		long worldPopulation = 729737380000000L;
+		System.out.printf("The world's population is %,d%n",worldPopulation);
+		
+		float myBalance = 67060742.8672F;
+		System.out.printf("My account balace is %.2f%n",myBalance);
+		
+		double cbnBalance = 5647485758475485754.587585;
+		Systenout.printf("The CBN balance is %,.2f%n",cbnBalance);
+		
+		char symbol = '@';
+		System.ot.printf("You email address is mercyben405%cgmail.com%n",symbol);
+		
+		boolean isJava = true;
+		system.out.printf("Do you love learning Java ? %b%n",isJava);
+	}
+} 
